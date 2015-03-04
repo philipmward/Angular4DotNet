ce5a639f6c4a5e9c09568218982c3bf15f2b27b5
philipmward@hotmail.com <philipmward@hotmail.com>
2015-03-04 12:59:51 -0700

    fixed refresh on /CreateAccount

--- Angular4DotNet/Angular4DotNet/App_Start/RouteConfig.cs
+++ Angular4DotNet/Angular4DotNet/App_Start/RouteConfig.cs
@@ -9,16 +9,21 @@ namespace Angular4DotNet
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            //can also do a {*catchall}, but that will not return a 404 if you go to a link that doesn't exist.
             routes.MapRoute(
                 name: "Registration Courses SPA",
-                url: "Registration/Courses",
+                url: "Registration/{*catchall}",
                 defaults: new { controller = "Registration", action = "Index" });
 
-            routes.MapRoute(
-                name: "Registration Instructors SPA",
-                url: "Registration/Instructors",
-                defaults: new { controller = "Registration", action = "Index" });
+            //doing a {*catchall} will not return a 404 if you go to a link that doesn't exist. To avoid that you can do do all separate like below.
+            //routes.MapRoute(
+            //    name: "Registration Courses SPA",
+            //    url: "Registration/Courses",
+            //    defaults: new { controller = "Registration", action = "Index" });
+
+            //routes.MapRoute(
+            //    name: "Registration Instructors SPA",
+            //    url: "Registration/Instructors",
+            //    defaults: new { controller = "Registration", action = "Index" });
 
             routes.MapRoute(
                 name: "Default",
